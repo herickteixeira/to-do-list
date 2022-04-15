5ebec5ee8aaaced7839a5c7870bd4cb6c52add8d
Herick Teixeira <herick_josue@hotmail.com>
2022-04-15 15:36:22 -0300

    test: deleteTaskById

---
+++ src/test/java/com/todolist/application/usecases/task/delete/DeleteTaskByIdUseCaseTest.java
@@ -0,0 +1,48 @@
+package com.todolist.application.usecases.task.delete;
+
+import com.todolist.domain.aggregates.task.TaskRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import shared.mocks.TaskMock;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+class DeleteTaskByIdUseCaseTest {
+
+    private TaskRepository taskRepository;
+
+    @BeforeEach
+    void setUp() {
+        taskRepository = Mockito.mock(TaskRepository.class);
+    }
+
+    @Test
+    @DisplayName("Delete task by id")
+    void shouldDeleteById() {
+        var useCase = new DeleteTaskByIdUseCase(taskRepository);
+        var task = TaskMock.create();
+        Mockito.when(taskRepository.findById(anyLong())).thenReturn(Optional.of(task));
+
+        useCase.execute(anyLong());
+
+        verify(taskRepository, times(1)).findById(anyLong());
+        verify(taskRepository, times(1)).delete(any());
+    }
+
+    @Test
+    @DisplayName("Don't return id")
+    void shouldNotReturnId() {
+        var useCase = new DeleteTaskByIdUseCase(taskRepository);
+        Mockito.when(taskRepository.findById(any()).isEmpty()).thenThrow(RuntimeException.class);
+
+        assertThrows(RuntimeException.class, () -> useCase.execute(any()));
+    }
+}
--- src/test/java/shared/mocks/TaskMock.java
+++ src/test/java/shared/mocks/TaskMock.java
@@ -5,11 +5,13 @@ import com.todolist.domain.aggregates.task.Task;
 
 public class TaskMock {
 
+    public static final long  id = 1L;
     public static final String title = "Criar uma API rest";
     public static final String description = "Utilizar a linguagem Java";
     public static final Priority priority = Priority.HIGH;
 
     public static Task create() {
+
         return Task.create(TaskMock.title, TaskMock.description, TaskMock.priority);
     }
 }
