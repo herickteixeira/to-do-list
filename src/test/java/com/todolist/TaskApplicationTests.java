0f3a7861f3ebca7c7f1b15d1f1c452faab12e058
herickteixeira <herick_josue@hotmail.com>
2022-04-27 18:55:31 -0300

    Specify the class (classes) you would like to test

--- src/test/java/com/todolist/TaskApplicationTests.java
+++ src/test/java/com/todolist/TaskApplicationTests.java
@@ -3,7 +3,7 @@ package com.todolist;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TaskApplicationTests.class)
 class TaskApplicationTests {
 
     @Test
