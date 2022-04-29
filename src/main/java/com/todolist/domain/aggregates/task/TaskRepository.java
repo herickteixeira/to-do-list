bc0d7ba9283fff2cbc9d9a7a53afe50344e32625
herickteixeira <herick_josue@hotmail.com>
2022-04-29 15:10:25 -0300

    remove deploy.yml

--- src/main/java/com/todolist/domain/aggregates/task/TaskRepository.java
+++ src/main/java/com/todolist/domain/aggregates/task/TaskRepository.java
@@ -1,8 +1,5 @@
 package com.todolist.domain.aggregates.task;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
