273e4f0b09e192b7f1c3dbe9df5c97d2476fea1f
Herick Teixeira <herickratai@gmail.com>
2024-05-16 14:53:08 -0300

    feat: create task repository

---
+++ src/main/java/com/todolist/domain/repositories/TaskRepository.java
@@ -0,0 +1,14 @@
+package com.todolist.domain.repositories;
+
+import com.todolist.domain.entities.Task;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskRepository {
+    Optional<Task> getTaskById(Long id);
+    List<Task> getAllTasks();
+    void save(Task task);
+    void update(Long id, Task task);
+    void delete(Long id);
+}
